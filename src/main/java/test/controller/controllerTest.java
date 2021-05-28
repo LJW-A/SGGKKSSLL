7f15e8aeab2cddf46c8512d252d1f8dd141dc492
QQintreing <leijunwei20191016@163.com>
2021-05-28 10:01:21 +0800

    代码全部提交aaa

---
+++ src/main/java/test/controller/controllerTest.java
@@ -0,0 +1,16 @@
+package test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class controllerTest {
+
+
+    @GetMapping("/testA")
+    @ResponseBody
+    public String testA(){
+        return "12345678";
+    }
+}
